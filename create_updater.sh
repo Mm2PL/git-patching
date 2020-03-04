2c0d01f59be0a0850e19313ac555a9193a1f6591
Mm2PL <jakis128@gmail.com>
2020-03-04 21:13:42 +0100

    Make the generic updaters update themselves

--- create_updater.sh
+++ create_updater.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-if (( $# != 2 ))
+if (( $# != 3 ))
 then
     echo This is an updater generator,
-    echo "Usage: $(basename $0) (PATCH NAME) (PATCH URL)"
+    echo "Usage: $(basename $0) (PATCH NAME) (PATCH URL) [UPDATER URL]"
+    echo "Note: UPDATER URL doesn't need to exist right now."
     exit 1
 fi
 
-name=$1
-url=$2
-
-cat > $1_updater.sh << EOF
+if (( $# == 2 ))
+then
+    name=$1
+    url=$2
+    
+    cat > $1_updater.sh << EOF
+#!/bin/bash
 if command -v curl
 then
     curl -L $url > custom_patches/$name
@@ -18,4 +22,21 @@ then
     wget -O - $url > custom_patches/$name
 fi
 EOF
-
+else
+    name=$1
+    url=$2
+    updater_url=$3
+    
+    cat > $1_updater.sh << EOF
+#!/bin/bash
+if command -v curl
+then
+    curl -L $url > custom_patches/$name
+    curl -L $updater_url > custom_patches/${name}_updater.sh
+elif command -v wget
+then
+    wget -O - $url > custom_patches/$name
+    wget -O - $updater_url > custom_patches/${name}_updater.sh
+fi
+EOF
+fi
